3b533cfc0d72864f1a4146a43304a39efc8613f4
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-06 19:39:18 +0000

    basic structure of the Hero function JSX added

--- shadcnui-app/components/sections/Hero.tsx
+++ shadcnui-app/components/sections/Hero.tsx
@@ -1,2 +1,22 @@
 import { Button } from '@/components/ui/button'
-import { Github, Linkedin, Mainl } from 'lucid-react'
+import { Github, Linkedin, Mail } from 'lucide-react'
+
+export default function Hero() {
+return (
+    <div>
+        <div>
+            <h1></h1>
+            <p>
+
+            </p>
+            <div>
+                <Button>
+                    <Mail/>
+                </Button>
+            </div>
+        </div>
+    </div>
+)
+
+
+}
