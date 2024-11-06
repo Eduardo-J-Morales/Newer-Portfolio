4a7d100ea6a3ccfd8d642bf0dfd79ca7462b328f
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-06 23:46:18 +0000

    Object iconMap for the icons in the Skills.tsx for a better scalability

---
+++ shadcnui-app/components/sections/Skills.tsx
@@ -0,0 +1,20 @@
+import { Progress } from "@/components/ui/progress"
+import { skills } from "@/app/data/content"
+import { Code, Component, Container, Cloud, Server, Terminal } from "lucide-react"
+
+const iconMap = {
+    code: Code, 
+    component: Component,
+    container: Container,
+    clould: Cloud,
+    server: Server,
+    terminal: Terminal
+};
+
+export default function Skills() {
+    return (
+        <section className="">
+            
+        </section>
+    )
+}
