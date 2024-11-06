4002a377ebf4628a9a8011577b0663e009f10ab0
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-06 23:38:06 +0000

    Updated projects.tsx component

--- shadcnui-app/components/sections/Projects.tsx
+++ shadcnui-app/components/sections/Projects.tsx
@@ -9,14 +9,39 @@ export default function Projects() {
         <section className="py-20 px-4" id="projects">
             <div className="container mx-auto">
                 <h2 className="text-3xl font-bold text-center mb-12">Feature Projects</h2>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
                     {projects.map((project, index) => (
                         <Card key={index} className="overflow-hidden">
 
                         <img 
                         src={project.image}
                         alt={project.title}
-                        className=""/>
+                        className="w-full h-48 object-cover"/>
+                        <CardHeader>
+                            <CardTitle>{project.title}</CardTitle>
+                            <CardDescription>{project.description}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                        <div className="flex flex-wrap gap-2">
+                        {project.tags.map((tag, i) => (
+                            <Badge key={i} variant='secondary'>{tag}</Badge>
+                        ))}
+                        </div>
+                        </CardContent>
+                        <CardFooter className='gap-2'>
+                            {project.github && (
+                                <Button variant="outline" size="sm">
+                                    <Github className="mr-2 h-4 w-4"></Github>
+                                    <strong>Code</strong>
+                                </Button>
+                            )}
+                            {project.link && (
+                                <Button >
+                                    <Link className="mr-2 h-4"/>
+                                    <strong>Demo</strong>
+                                </Button>
+                            )}
+                        </CardFooter>
                         </Card>
                     ))}
                 </div>
