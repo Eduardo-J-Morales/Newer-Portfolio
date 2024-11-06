7ebe7efeb79d41649f122de492cf0bcd5bf59026
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-06 23:15:19 +0000

    Mapping the projects

--- shadcnui-app/components/sections/Projects.tsx
+++ shadcnui-app/components/sections/Projects.tsx
@@ -1,4 +1,8 @@
-import { Card, CardC}
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import { projects } from "@/app/data/content"
+import { Github, Link } from "lucide-react"
 
 export default function Projects() {
     return(
@@ -6,7 +10,15 @@ export default function Projects() {
             <div className="container mx-auto">
                 <h2 className="text-3xl font-bold text-center mb-12">Feature Projects</h2>
                 <div>
+                    {projects.map((project, index) => (
+                        <Card key={index} className="overflow-hidden">
 
+                        <img 
+                        src={project.image}
+                        alt={project.title}
+                        className=""/>
+                        </Card>
+                    ))}
                 </div>
             </div>
         </section>
