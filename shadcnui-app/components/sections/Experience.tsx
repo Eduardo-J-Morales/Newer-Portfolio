78be467578222902bc4f01aa93ecfb020b71ea49
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-07 12:27:58 +0000

    Mapping the experience obj in data folder

--- shadcnui-app/components/sections/Experience.tsx
+++ shadcnui-app/components/sections/Experience.tsx
@@ -3,10 +3,10 @@ import { experiences } from '@/app/data/content'
 
 export default function Experience() {
     return(
-    <section className='py-20'>
-        <div>
-            <h2></h2>
-            <div>
+    <section className='py-20 px-4' id='experience'>
+        <div className='container mx-auto'>
+            <h2 className='text-3xl font-bold text-center mb-12'>Experience</h2>
+            <div className='max-w-3xl mx-auto space-y-6'>
                 {}
             </div>
         </div>
