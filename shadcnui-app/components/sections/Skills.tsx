a62085cd59d28f0678d86691e8e80b87c8717bbf
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-07 00:38:23 +0000

    Details

--- shadcnui-app/components/sections/Skills.tsx
+++ shadcnui-app/components/sections/Skills.tsx
@@ -5,18 +5,31 @@ import { Code, Component, Container, Cloud, Server, Terminal } from "lucide-reac
 export default function Skills() {
     return (
         <section className="py-20 bg-secondary/100" id="skills">
+            
             <div className="container mx-auto px-4">
+            
                 <h2 className="text-3xl font-bold text-center mb-12">Skills</h2>
+            
                 <div className="gird grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto"></div>
+            
                 {skills.map((skill, index) => (
-                        <div key={index} className="space-y-2">
+            
+            <div key={index} className="space-y-2">
+            
                                 <div className="flex items-center gap-2">
+            
                                 <span>{skill.name}</span>
-                            </div>
-                            <Progress/>
-                        </div>
-                    ))}
+            
+                                </div>
+            
+                            <Progress className="h-2" value={skill.level} />
+            
+            </div>
+            
+            ))}
+            
                 </div>
+        
         </section>
     )
 }
