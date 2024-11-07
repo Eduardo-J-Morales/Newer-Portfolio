dc629ec246c2bf814eb0ff2a355a781cb94af669
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-07 12:45:10 +0000

    Taking a break

--- shadcnui-app/components/sections/Contact.tsx
+++ shadcnui-app/components/sections/Contact.tsx
@@ -1,7 +1,11 @@
-import { Card, CardContent } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+import {} from '@/components/ui/card'
+import {} from '@/components/ui/input'
+import {} from '@/components/ui/textarea'
+import {} from 'lucide-react'
 
 export default function Contact() {
     return(
-        <></>
+        
     )
 }
