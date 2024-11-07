76721bb4a312372fa26959270f2d6314c4fe0c9f
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2024-11-07 00:22:26 +0000

    This objects are a headache

--- shadcnui-app/components/sections/Skills.tsx
+++ shadcnui-app/components/sections/Skills.tsx
@@ -13,9 +13,23 @@ const iconMap = {
 
 export default function Skills() {
     return (
-        <section className="py-20 bg-secondary/50" id="skills">
-            <div className="container mx-auto px-4"></div>
-
+        <section className="py-20 bg-secondary/100" id="skills">
+            <div className="container mx-auto px-4">
+                <h2 className="text-3xl font-bold text-center mb-12">Skills</h2>
+                <div className="gird grid-cols-1 md:grid-cols-2 gap-8 max-w-4xl mx-auto"></div>
+                {skills.map((skill, index) => {
+                    let { icon } = skill
+                    let Icon = icon
+                    return(
+                    <div key={index} className="space-y-2">
+                                <div className="flex items-center gap-2">
+                                <Icon/>
+                                <span>{skill.name}</span>
+                            </div>
+                            <Progress/>
+                        </div>
+                )})}
+                </div>
         </section>
     )
 }
